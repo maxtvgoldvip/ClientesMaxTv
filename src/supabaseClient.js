4d9f5a8d864326b75307772e1b69c3ce92afe8c8
maxtvgoldvip <maxtvgoldvip@gmail.com>
2026-08-16 01:22:22 -0300

    Delete src directory

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://wnjtbsytxcmvolyhhdqp.supabase.co'
-const supabaseAnonKey = 'sb_publishable_kDQbhebKLBotK_H484CcRA_6Gc41ugz'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
